dc45df1ec07aa105c5f30381f3965724d4f739f1
turana1998 <turana221298@gmail.com>
2021-12-18 14:57:34 +0300

    first commit

---
+++ __/firebase/init.js
@@ -0,0 +1,10 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyAiijhWmxO4BPYw4ge_cQAbReZzvszPe7E",
+  "appId": "1:11517517770:web:a41c0ed16bd3acc8273933",
+  "authDomain": "testingchatbot-jefqvx.firebaseapp.com",
+  "databaseURL": "https://testingchatbot-jefqvx.firebaseio.com",
+  "messagingSenderId": "11517517770",
+  "projectId": "testingchatbot-jefqvx",
+  "storageBucket": "testingchatbot-jefqvx.appspot.com"
+});
